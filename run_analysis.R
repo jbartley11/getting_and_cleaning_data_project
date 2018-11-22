2481abaed811c221c70fa2b0ad0b2e559542899c
Jason Bartley <jbartley11@gmail.com>
2018-11-22 09:50:17 -0600

    finished step 1

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,21 @@
 # description
 
 # set working directory
-setwd("Users/jasonbartley/Development/courses/coursera_get_clean_data/getting_and_cleaning_data_project")
+setwd("Development/courses/coursera_get_clean_data/getting_and_cleaning_data_project")
+
+# 1. Merge the training and the test sets to create one data set
+
+# read training data
+x_train <- read.table( "./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table( "./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table( "./UCI HAR Dataset/train/subject_train.txt")
+
+# read test data
+x_test <- read.table( "./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table( "./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table( "./UCI HAR Dataset/test/subject_test.txt")
+
+# merge data sets
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
